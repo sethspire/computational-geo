043dde1d5c0580db1298b03c84d2942319de6eb4
sethspire <sspire2@eagles.bridgewater.edu>
2023-01-17 23:46:37 -0500

    initial hbs and router setup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const path = require('path')
+const hbs = require('hbs')
 
-app.get('', (req, res) => {
-    res.send('Hello World!')
-})
+const indexRouter = require('./routers/index')
+const _404Router = require('./routers/404')
+
+const app = express()
+
+const dir = path.join(__dirname, "../public")
+app.use(express.static(dir))
+
+app.set('view engine', 'hbs')
+
+const viewsPath = path.join(__dirname, "./templates")
+app.set('views', viewsPath)
+
+const partialsPath = path.join(__dirname, "./templates/partials")
+hbs.registerPartials(partialsPath)
+
+app.use(indexRouter)
+app.use(_404Router)
+
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
